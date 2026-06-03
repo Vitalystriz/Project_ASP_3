991e8ad6c261046533c8d879615b29b5a725e5f3
MikaLevAri <mikalevari1@gmail.com>
2026-06-03 22:56:50 +0300

    connection to our cpp server from ass2

--- src/app.js
+++ src/app.js
@@ -1,27 +1,13 @@
-const net = require('net');
+const express = require('express')
+const app = express()
+const usersRouter = require('./routes/users')
+const tokensRouter = require('./routes/tokens')
 
+app.use(express.json())
 
-const SERVER_HOST = 'server';
-const SERVER_PORT = 8080;
+app.use('/api/users', usersRouter)
+app.use('/api/tokens', tokensRouter)
 
-console.log(`[Node.js] Let's try to connect to server  ${SERVER_HOST}:${SERVER_PORT}...`);
+const PORT = process.env.PORT || 3000 //עושים או שלושת-אלפים למתי שאני מריצה את הפרויקט לוקאלית
 
-const client = net.createConnection({ host: SERVER_HOST, port: SERVER_PORT }, () => {
-    console.log('[Node.js] Successfully connected!');
-
-
-    client.write('HELLO FROM NODE\n');
-});
-
-client.on('data', (data) => {
-    console.log('[Node.js] I got server response:', data.toString());
-    client.end(); // Закрываем соединение
-});
-
-client.on('error', (err) => {
-    console.error('[Node.js] Error', err.message);
-});
-
-client.on('end', () => {
-    console.log('[Node.js] Closed');
-});
+app.listen(PORT)
